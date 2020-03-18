9ce302d66b113571e449bd874539c5a177e8594a
jaymehta-ce <jaymehta.ce@gmail.com>
2020-03-18 16:47:01 +0530

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,18 +1,19 @@
  var mocha = require('mocha')
   var describe = mocha.describe
   var it = mocha.it
-  var assert = require('chai').assert
-console.log("mocha.....")
-
+  var assert = require('chai').assert;
+      const fs = require('fs')
 describe('check', function() {
     it('swagger file exists or not', function() {
-      const fs = require('fs')
       const path = '../../swagger1.json'
       try {
         if (fs.existsSync(path)) {
           //file exists
-         assert.throw(iThrowError('Swagger file not found'), Error, 'Error thrown');
-        }
+         }
+       else
+       {
+        assert.throw(iThrowError('Swagger file not found'), Error, 'Error thrown');
+       }
       } catch(err) {
         console.error(err)
       }
