5e46461110096d79249920879f6b40e3789856dc
jaymehta-ce <jaymehta.ce@gmail.com>
2020-03-18 16:59:35 +0530

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,16 @@
-//  var mocha = require('mocha')
-//   var describe = mocha.describe
-//   var it = mocha.it
-//   var assert = require('chai').assert;
-
 const assert = require('assert');
 const fs = require('fs');
 
-  function iThrowError(msg) {
-     throw new Error(msg);
-  }
+function iThrowError(msg) {
+    throw new Error(msg);
+}
 describe('check', function() {
     it('swagger file exists or not', function() {
-      const path = '../../swagger1.json';
+        const path = '../../swagger.json';
         if (fs.existsSync(path)) {
-           done();
-         }
-       else
-       {
-        assert.throw(iThrowError('Swagger file not found'), Error, 'Error thrown');
-       }
+            done();
+        } else {
+            assert.throw(iThrowError('Swagger file not found'), Error, 'Error thrown');
+        }
     })
-  })
+})
