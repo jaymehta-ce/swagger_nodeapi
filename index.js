24bcdd580807258e2ce625c640c752b90960cb50
jaymehta-ce <jaymehta.ce@gmail.com>
2020-03-18 09:37:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ var bodyParser = require("body-parser");
 require('dotenv')
 const route = require("./routes/route");
 var app = express();
+ var mocha = require('mocha')
+  var describe = mocha.describe
+  var it = mocha.it
+  var assert = require('chai').assert
+console.log("mocha.....")
+
+
+  describe('#indexOf()', function() {
+    it('should return -1 when not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1)
+    })
+  })
+
 // parse application/x-www-form-urlencoded
 // for easier testing with Postman or plain HTML forms
 app.use(bodyParser.urlencoded({
