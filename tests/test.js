83f5921ced87626548ecea8623d09e64516bbea2
jaymehta-ce <jaymehta.ce@gmail.com>
2020-03-18 17:27:25 +0530

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -4,12 +4,9 @@ const fs = require('fs');
 function iThrowError(msg) {
     throw new Error(msg);
 }
-var swagger = require("../swagger.json");
-console.log(swagger)
-
 describe('check', function() {
     it('swagger file exists or not', function() {
-        const path = '../swagger.json';
+        const path = '././swagger.json';
         console.log("--------------------------");
         console.log(fs.existsSync(path));
         if (fs.existsSync(path)) {
