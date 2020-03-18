d76e0973cb06978296772e7673a3e64f272a4b2c
jaymehta-ce <jaymehta.ce@gmail.com>
2020-03-18 18:23:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+// index.js
+const express = require("express");
+
+// body parsing middleware.
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
+const bodyParser = require("body-parser");
+
+// loads environment variables from a .env file into process.env
 require('dotenv')
+
+// register route from routes/route.js file.
 const route = require("./routes/route");
 var app = express();
- var mocha = require('mocha')
-console.log("mocha.....")
-
-
-//   describe('#indexOf()', function() {
-//     it('should return -1 when not present', function() {
-//       assert.equal([1,2,3].indexOf(4), -1)
-//     })
-//   })
 
 // parse application/x-www-form-urlencoded
 // for easier testing with Postman or plain HTML forms
@@ -21,6 +20,8 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 app.use(route);
+
+// start server
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Express running");
