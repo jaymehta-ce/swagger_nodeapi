308e4e9c3298b3d97c06dc5857c66e059423a490
jaymehta-ce <jaymehta.ce@gmail.com>
2020-03-18 09:27:52 +0530

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -4,11 +4,7 @@
   var assert = require('chai').assert
 console.log("mocha.....")
 
-mocha.ui('bdd').run(function (failures) {
-    process.on('exit', function () {
-      process.exit(failures);
-    });
-  });
+
   describe('#indexOf()', function() {
     it('should return -1 when not present', function() {
       assert.equal([1,2,3].indexOf(4), -1)
